7eea2533653ada7868befe503b1ba84b46c59b38
Rick M <2545827+rick-paaz@users.noreply.github.com>
2023-10-31 16:54:03 -0700

    Update Queue210.java

--- src/Queue210.java
+++ src/Queue210.java
@@ -1,6 +1,6 @@
 /**
- * This collection class model a queue. When attempting to add new elements and the
- * queue is full or dequeue from an empty OurQueue, throw new CapacityException();
+ * This collection class models a queue ADT. When attempting to dequeue from or
+ * peek at an empty Queue210, throw new CapacityException();
  */
 public class Queue210<Type> implements OurQueue<Type> {
 
